33d175a16f2dd8de3e1fb630e83033e54a5be5a6
mirsella <mirsella@protonmail.com>
2022-02-02 14:57:55 +0100

    Wed Feb  2 02:57:55 PM CET 2022

--- client/android/app/src/main/java/com/mirsella/powercontrol/EchoPlugin.java
+++ client/android/app/src/main/java/com/mirsella/powercontrol/EchoPlugin.java
@@ -6,15 +6,15 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 
-// import android.net.ConnectivityManager;
-// import android.net.NetworkInfo;
-// import android.net.wifi.WifiManager;
-// import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.content.Context;
 
 @CapacitorPlugin(name = "Echo")
 public class EchoPlugin extends Plugin {
 
-  // ConnectivityManager connectivity = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+  ConnectivityManager connectivity = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
   // Network[] networks = connectivity.getAllNetworks();
   // for (int i = 0; i < networks.length; i++) {
   //   NetworkCapabilities capabilities = connectivity.getNetworkCapabilities(networks[i]);    
@@ -25,7 +25,7 @@ public class EchoPlugin extends Plugin {
     String value = call.getString("value");
 
     JSObject ret = new JSObject();
-    ret.put("value", value + " " + output);
+    ret.put("value", value + " ");
     call.resolve(ret);
   }
 }
