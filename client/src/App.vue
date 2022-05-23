e70e13f83e314196f89fb419a74465fa441bbb19
mirsella <mirsella@protonmail.com>
2022-05-23 16:58:31 +0200

    add animation on search

--- client/src/App.vue
+++ client/src/App.vue
@@ -88,6 +88,7 @@ function savelocalstorage () {
 
 onMounted(() => searchIP())
 function searchIP() {
+  document.querySelector('#refresh')?.classList.add('animate-spin')
   Wifi.getAllIP()
     .then((e: object) => {
       const ips = Object.values(e)
@@ -115,6 +116,7 @@ function searchIP() {
       }  
     })
   })
+  document.querySelector('#refresh')?.classList.remove('animate-spin')
 }
 
 function setnextboot(presetName: "windows" | "linux") {
@@ -174,7 +176,7 @@ function power(action: "power" | "reset" | "reboot") {
   <header :class="connectedStyle.header" class="inline-flex justify-center items-center fixed top-0 h-4rem w-screen text-center transition">
     <span class="mx-1rem break-words dark:text-white text-black max-w-[70%]">{{ connected ? `${connected}` : "disconnected" }}</span>
     <button :class="connectedStyle.button" class="rounded px-6 h-3/5 transition" @click="searchIP">
-      <svg class="dark:fill-white fill-black" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" ><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><path d="M12,5V2L8,6l4,4V7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02c3.95-0.49,7-3.85,7-7.93C20,8.58,16.42,5,12,5z"/><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z"/></g></g></svg>
+      <svg id="refresh" class="dark:fill-white fill-black" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" ><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><g><path d="M12,5V2L8,6l4,4V7c3.31,0,6,2.69,6,6c0,2.97-2.17,5.43-5,5.91v2.02c3.95-0.49,7-3.85,7-7.93C20,8.58,16.42,5,12,5z"/><path d="M6,13c0-1.65,0.67-3.15,1.76-4.24L6.34,7.34C4.9,8.79,4,10.79,4,13c0,4.08,3.05,7.44,7,7.93v-2.02 C8.17,18.43,6,15.97,6,13z"/></g></g></svg>
     </button>
   </header>
  
