f67b98954858f1aff8f7657540d4eacbf7b8a44d
mirsella <mirsella@protonmail.com>
2022-05-31 20:09:40 +0200

    Tue May 31 08:09:40 PM CEST 2022

--- client/android/app/src/main/java/com/mirsella/powercontrol/MainActivity.java
+++ client/android/app/src/main/java/com/mirsella/powercontrol/MainActivity.java
@@ -1,4 +1,4 @@
-package mirsella.powercontrol;
+package com.mirsella.powercontrol;
 
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
@@ -28,29 +28,7 @@ public class MainActivity extends BridgeActivity {
     //   finish();
     // }
     // if (Intent.ACTION_SEND.equals(action) && type != null) {
-      bridge.getActivity().setIntent(intent);
-      bridge.triggerJSEvent("testintent", "window", "{'dataKey':'dataValue'}");
-      // bridge.eval("window.dispatchEvent(new Event('testintent'))", new ValueCallback<String>() {
-      //   @Override
-      //   public void onReceiveValue(String s) {
-      //   }
-      // });
-    // }
-
+    bridge.getActivity().setIntent(intent);
+    bridge.triggerJSEvent("testintent", "window",  scheme);
+    }
   }
-
-  // @Override
-  // protected void onNewIntent(Intent intent) {
-  //   super.onNewIntent(intent);
-  //   String action = intent.getAction();
-  //   String type = intent.getType();
-  //   if (Intent.ACTION_SEND.equals(action) && type != null) {
-  //     bridge.getActivity().setIntent(intent);
-  //     bridge.eval("window.dispatchEvent(new Event('testintent'))", new ValueCallback<String>() {
-  //       @Override
-  //       public void onReceiveValue(String s) {
-  //       }
-  //     });
-  //   }
-  // }
-}
