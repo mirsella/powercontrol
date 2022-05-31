6265c5ef8a0e33f87d164805e7ad4e4ff2e6b67c
mirsella <mirsella@protonmail.com>
2022-05-31 20:01:05 +0200

    Tue May 31 08:01:04 PM CEST 2022

--- client/android/app/src/main/java/com/mirsella/powercontrol/MainActivity.java
+++ client/android/app/src/main/java/com/mirsella/powercontrol/MainActivity.java
@@ -1,11 +1,11 @@
-package com.mirsella.powercontrol;
+package mirsella.powercontrol;
 
 import com.getcapacitor.BridgeActivity;
 import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
-import com.getcapacitor.bridge;
+// import com.getcapacitor.bridge;
 
 import android.content.Intent;  
 
@@ -13,6 +13,30 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+  }
+
+  // get url scheme intent
+  public void onResume() {
+    super.onResume();
+    Intent intent = getIntent();
+    String action = intent.getAction();
+    String scheme = intent.getScheme();
+    String type = intent.getType();
+    // if (Intent.ACTION_VIEW.equals(action) && scheme.equals("file")) {
+    //   String filePath = intent.getDataString();
+    //   Plugin.getInstance().getBridge().getEventEmitter().emit("urlSchemeOpen", filePath);
+    //   finish();
+    // }
+    // if (Intent.ACTION_SEND.equals(action) && type != null) {
+      bridge.getActivity().setIntent(intent);
+      bridge.triggerJSEvent("testintent", "window", "{'dataKey':'dataValue'}");
+      // bridge.eval("window.dispatchEvent(new Event('testintent'))", new ValueCallback<String>() {
+      //   @Override
+      //   public void onReceiveValue(String s) {
+      //   }
+      // });
+    // }
+
   }
 
   // @Override
