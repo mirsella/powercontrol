b3260d97024151d9cac8c573489fb0bd9896d521
mirsella <mirsella@protonmail.com>
2023-03-28 21:38:16 +0200

    add a way to check if the host pc is online or not with ping

--- server/routes.ts
+++ server/routes.ts
@@ -1,51 +1,63 @@
 import config from './config';
 import * as relay from './relay';
 import express from 'express'
+const ping = require('ping')
 
 module.exports = (app: express.Application) => {
 
-  app.all('/', (_req, res) => {
-    res.send("powercontrol")
-  })
+	app.all('/', (_req, res) => {
+		res.send("powercontrol")
+	}),
 
-  app.all('/getnextboot', (_req, res) => {
-    res.json(config.nextboot)
-  })
+	app.all('/getnextboot', (_req, res) => {
+		res.json(config.nextboot)
+	}),
 
-  app.all('/setnextboot', (req, res) => {
-    if (config.setnextboot(req.body)) {
-      res.send(req.body)
-    } else {
-      res.status(400)
-      res.send("invalid config")
-    }
-  })
+	app.all('/setnextboot', (req, res) => {
+		if (config.setnextboot(req.body)) {
+			res.send(req.body)
+		} else {
+			res.status(400)
+			res.send("invalid config")
+		}
+	}),
 
-  app.all('/getpins', (_req, res) => {
-    res.json(config.pins)
-  })
+	app.all('/getpins', (_req, res) => {
+		res.json(config.pins)
+	}),
 
-  app.all('/setpins', (req, res) => {
-    if (config.setpins(req.body)) {
-      res.send(req.body)
-    } else {
-      res.status(400)
-      res.send("invalid config")
-    }
-  })
+	app.all('/setpins', (req, res) => {
+		if (config.setpins(req.body)) {
+			res.send(req.body)
+		} else {
+			res.status(400)
+			res.send("invalid config")
+		}
+	}),
 
-  app.all('/power', (_req, res) => {
-    relay.power()
-    res.send("power")
-  })
+	app.all('/power', (_req, res) => {
+		relay.power()
+		res.send("power")
+	}),
 
-  app.all('/reset', (_req, res) => {
-    relay.reset()
-    res.send("reset")
-  }),
+	app.all('/reset', (_req, res) => {
+		relay.reset()
+		res.send("reset")
+	}),
 
-  app.all('/reboot', (_req, res) => {
-    relay.reboot()
-    res.send("reboot")
-  })
+	app.all('/reboot', (_req, res) => {
+		relay.reboot()
+		res.send("reboot")
+	}),
+
+	app.all('/host_online', (_req, res) => {
+		ping.sys.probe(config.host_ip, (isAlive: boolean, error: object) => {
+			if (error)
+				{
+					res.status(400).send(error)
+					return
+				}
+				res.send(isAlive)
+		})
+	})
 }
