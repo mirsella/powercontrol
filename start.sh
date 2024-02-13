9fa12634eab0a9a998a8d8e5beff291739fe21e3
Lucas <mirsella@protonmail.com>
2024-02-13 23:27:29 +0100

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,27 +1,26 @@
 #!/bin/sh
 
-path="/home/pi/powercontrol"
-log="/home/pi/powercontrol.log"
+path="$USER/powercontrol"
+log="$USER/powercontrol.log"
 
 cd $path || exit
 git add -A
 git stash
-/home/pi/bin/waitfornetwork && git pull | rg 'changed'
-new=$?
-if [ "$new" -eq 0 ]; then
+until ping 1.1.1.1 -c 1; do sleep 1; done
+git pull | rg 'changed'
+if [ $? -eq 0 ]; then
 	echo "New commit, rebuilding..."
 
 	cd $path/server || exit
 	npm i
 	npm run build
 
-	cd $path/client || exit
-	npm i
-	npm run build
-	sudo cp -r dist/* /var/www/html/powerclient
-	sudo chmod -R 755 /var/www/html/powerclient
+	#cd $path/client || exit
+	#npm i
+	#npm run build
+	#sudo cp -r dist/* /var/www/html/powerclient
+	#sudo chmod -R 755 /var/www/html/powerclient
 fi
 
 cd $path/server || exit
 npm run start | awk "{print \"$(date) \" \$0}" 2>&1 | tee -a $log
-[ "$?" -ne 0 ] && /home/pi/bin/notif "powercontrol crashed $?"
