99630bb5e05a5720ccb29726b2ef4bd768bb951e
mirsella <mirsella@protonmail.com>
2024-12-31 16:33:57 +0100

    dont git pull when starting as the device FS is readonly anyway

--- start.sh
+++ start.sh
@@ -6,21 +6,21 @@ cd "$path" || exit
 
 sudo bash setup-hid-device.bash || exit
 
-git add -A
-git stash
-until ping 1.1.1.1 -c 1; do sleep 1; done
+# git add -A
+# git stash
+# until ping 1.1.1.1 -c 1; do sleep 1; done
 
-if git pull | grep -q 'changed'; then
-	echo "New commit, rebuilding and relaunching $0"
-
-	(
-		"$path"/server || exit
-		npm i
-		npm run build
-	)
-
-	sh "$0"
-else
-	cd "$path"/server || exit
-	npm run start
-fi
+# if git pull | grep -q 'changed'; then
+# 	echo "New commit, rebuilding and relaunching $0"
+#
+# 	(
+# 		"$path"/server || exit
+# 		npm i
+# 		npm run build
+# 	)
+#
+# 	sh "$0"
+# else
+cd "$path"/server || exit
+npm run start
+# fi
